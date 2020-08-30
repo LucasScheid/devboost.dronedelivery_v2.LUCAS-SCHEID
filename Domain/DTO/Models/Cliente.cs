10d3e47ebbf13406e4bd388450e8fa3771916c44
Lucas Scheid <lucas.scheid@penske.com>
2020-08-29 22:25:32 -0300

    Initial Commit

---
+++ Domain/DTO/Models/Cliente.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace devboost.dronedelivery.felipe.DTO.Models
+{
+    public class Cliente
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
+    }
+}
---
+++ Infra/Repositories/ClienteRepository.cs
@@ -0,0 +1,36 @@
+﻿using devboost.dronedelivery.felipe.DTO.Models;
+using devboost.dronedelivery.felipe.EF.Data;
+using devboost.dronedelivery.felipe.EF.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace devboost.dronedelivery.felipe.EF.Repositories
+{
+    public class ClienteRepository : IClienteRepository
+    {
+        private readonly DataContext _context;
+
+        public ClienteRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public Cliente GetCliente(int id)
+        {
+            return _context.Find<Cliente>(id);
+        }
+
+        public IEnumerable<Cliente> GetClientes()
+        {
+            return _context.Cliente.AsNoTracking<Cliente>();
+        }
+
+        public async Task SaveCliente(Cliente cliente)
+        {
+            _context.Cliente.Add(cliente);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
